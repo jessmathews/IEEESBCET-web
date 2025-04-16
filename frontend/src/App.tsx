cc20acf241e416fda8af4392bf67125ffa3ae6a6
Jess <jessmathews190@gmail.com>
2025-04-17 00:07:08 +0530

    fix routing issue of github pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { HomePage } from "./pages/HomePage";
 import { EventsPage } from "./pages/EventsPage";
 import { UploadEventPage } from "./pages/UploadEventPage";
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -14,6 +14,6 @@ export default function App() {
           <Route path="events/upload" element={<UploadEventPage />} />
       </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
