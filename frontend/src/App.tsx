d73f98c38c2ef04bde59e56850e922254f7d9ea9
Jess <jessmathews190@gmail.com>
2025-04-16 21:49:05 +0530

    fix: Improve formatting and readability in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { HomePage } from './pages/HomePage';
-import { EventsPage } from './pages/EventsPage';
-import { UploadEventPage } from './pages/UploadEventPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { HomePage } from "./pages/HomePage";
+import { EventsPage } from "./pages/EventsPage";
+import { UploadEventPage } from "./pages/UploadEventPage";
 export default function App() {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="events" element={<EventsPage />} />
           <Route path="events/upload" element={<UploadEventPage />} />
-        </Route>
+      </Route>
       </Routes>
-    </BrowserRouter>;
+    </BrowserRouter>
+  );
 }
